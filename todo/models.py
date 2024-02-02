a349d755a5792ba38358f1d6e79b0745e024ba37
yana <y.kapylova@gmail.com>
2024-02-02 18:16:37 +0300

    create models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
